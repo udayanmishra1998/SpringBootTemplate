5ea0bb73e3e209cb1325a4343f7cd13e123c6eed
hyperkd <1606513@kiit.ac.in>
2022-03-26 12:05:21 +0530

    Test Commit

--- microservice/src/main/java/com/microservice/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/microservice/microservice/MicroserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hi sir");
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
 
